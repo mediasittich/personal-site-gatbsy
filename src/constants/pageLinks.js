e4252bbfe4c643eb66e6141b3931bf2d0d90c21c
Regina Galambos <gina.galambos@gmail.com>
2020-07-21 13:16:21 +0200

    add internal page links as constants

---
+++ src/constants/pageLinks.js
@@ -0,0 +1,39 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const DATA = [
+  {
+    id: 1,
+    text: "home",
+    url: "/",
+  },
+  {
+    id: 2,
+    text: "about",
+    url: "/about/",
+  },
+  {
+    id: 3,
+    text: "blog",
+    url: "/blog/",
+  },
+  {
+    id: 4,
+    text: "projects",
+    url: "/projects/",
+  },
+]
+
+const LINKS = DATA.map(link => {
+  return (
+    <li key={link.id}>
+      <Link to={link.url}>{link.text}</Link>
+    </li>
+  )
+})
+
+export default ({ styleClass }) => {
+  return (
+    <ul className={`page-links ${styleClass ? styleClass : ""}`}>{LINKS}</ul>
+  )
+}
