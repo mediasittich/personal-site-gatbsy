1a0a526076c2d2ee2763403f05d4cabf14997075
Regina Galambos <gina.galambos@gmail.com>
2020-07-23 02:09:55 +0200

    install sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
